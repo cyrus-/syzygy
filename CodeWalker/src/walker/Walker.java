a89af0759235e84bf998b5d0933b915ed4422b5c
Flávio Cruz <flaviocruz@gmail.com>
2012-11-03 22:15:16 -0400

    Remove this code.

--- CodeWalker/src/walker/Walker.java
+++ CodeWalker/src/walker/Walker.java
@@ -14,45 +14,7 @@ import dir.JavaFile;
 
 public class Walker {
 	
-	private static IJavaProject project = null;
-	
-	private static Directory traverseDir(File dir)
-	{
-		Directory currentDir = new Directory();
-		File[] files = dir.listFiles();
-		
-		for (File child : files) {
-			if (!child.isDirectory()) {
-				continue;
-			}
-			
-			currentDir.addChild(traverseDir(child));
-		}
-		
-		// add child files
-		for (File child : files) {
-			if (child.isFile() && child.getName().toLowerCase().endsWith(".java")) {
-				currentDir.addChild(new JavaFile(child, null));
-			}
-		}
-		
-		return currentDir;
-	}
-
 	public static void main(String[] args)
 	{
-		String projectName = args[0];
-		
-		IProject proj = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-		System.out.println(proj.getLocation());
-		JavaCore.create(proj);
-		
-		//Directory dir = traverseDir(new File(projectName));
-		
-		//LiteralVisitor lit = new LiteralVisitor();
-		
-		//dir.accept(lit);
-		
-		//lit.print();
 	}
 }
