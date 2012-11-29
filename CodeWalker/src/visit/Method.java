44532e2c227912fc31de6debf26fd57a93405d3b
Flávio Cruz <flaviocruz@gmail.com>
2012-11-29 17:12:36 -0500

    Added Method.java.

---
+++ CodeWalker/src/visit/Method.java
@@ -0,0 +1,76 @@
+package visit;
+
+import java.io.Serializable;
+
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+
+public class Method implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String className;
+	private String name;
+	private String returnType;
+	private String []argumentTypes;
+	
+	public boolean equal(Object obj)
+	{
+		if(obj instanceof IMethodBinding) {
+			IMethodBinding meth = (IMethodBinding)obj;
+			
+			if(!meth.getDeclaringClass().getQualifiedName().equals(className))
+				return false;
+			
+			if(!meth.getName().equals(name))
+				return false;
+			
+			ITypeBinding []typs = meth.getParameterTypes();
+			int i = 0;
+			for(ITypeBinding typ : meth.getParameterTypes()) {
+				if(!typ.getQualifiedName().equals(argumentTypes[i]))
+					return false;
+				++i;
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString()
+	{
+		String base = returnType + " " + className + "." + name + "(";
+		
+		boolean first = true;
+		
+		for(String arg : argumentTypes) {
+			if(first) {
+				first = false;
+				base = base + arg;
+			} else
+				base = base + ", " + arg;
+		}
+		
+		base = base + ")";
+		
+		return base;
+	}
+	
+	public Method(IMethodBinding meth)
+	{
+		className = meth.getDeclaringClass().getQualifiedName();
+		returnType = meth.getReturnType().getQualifiedName();
+		ITypeBinding []typs = meth.getParameterTypes();
+		name = meth.getName();
+		
+		argumentTypes = new String[typs.length];
+		
+		int i = 0;
+		for(ITypeBinding typ : meth.getParameterTypes()) {
+			argumentTypes[i] = typ.getQualifiedName();
+			++i;
+		}
+	}
+}
