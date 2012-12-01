c53ade2716c8ec300db4575fe9dabce4b0373853
Flávio Cruz <flaviocruz@gmail.com>
2012-12-01 17:02:31 -0500

    Update planet project.

--- planet/src/main/Main.java
+++ planet/src/main/Main.java
@@ -1,16 +1,6 @@
+package main;
 
 public class Main {
-
-   public enum Planet {
-      MERCURY,
-      VENUS,
-      EARTH,
-      MARS,
-      JUPITER,
-      SATURN,
-      URANUS,
-      NEPTUNE
-   }
 
    public static int number() {
       return 7; /// MISSING
@@ -20,12 +10,12 @@ public class Main {
       return "This is a string";
    }
 
-   public static double method(double flt, int n, Planet plt) {
+   public static double method(double flt, int n, Planets.Planet plt) {
       return (double)n * flt;
    }
 
-   public static Planet somePlanet() {
-      return Planet.VENUS;
+   public static Planets.Planet somePlanet() {
+      return Planets.Planet.VENUS;
    }
 
    public static void main(String[] args) {
@@ -36,13 +26,13 @@ public class Main {
       double f = 20.5;
       double f2 = 20.5;
       float flt = (float) 14.5;
-      Planet mars = Planet.MARS;
-      Planet mars2 = Planet.MARS;
-      Planet earth = Planet.EARTH;
-      Planet copy = earth;
-      double f3 = method(f, c, Planet.JUPITER);
+      Planets.Planet mars = Planets.Planet.MARS;
+      Planets.Planet mars2 = Planets.Planet.MARS;
+      Planets.Planet earth = Planets.Planet.EARTH;
+      Planets.Planet copy = earth;
+      double f3 = method(f, c, Planets.Planet.JUPITER);
 
-      method(3.14, 2, Planet.EARTH);
+      method(3.14, 2, Planets.Planet.EARTH);
       method(1.0, 1, somePlanet());
       
       if(f3 == 1.0) {
---
+++ planet/src/main/Planets.java
@@ -0,0 +1,42 @@
+package main;
+
+public class Planets {
+	public static enum Planet {
+	      MERCURY,
+	      VENUS,
+	      EARTH,
+	      MARS,
+	      JUPITER,
+	      SATURN,
+	      URANUS,
+	      NEPTUNE
+	   }
+	
+	public static Planets.Planet somePlanet() {
+	      return Planets.Planet.VENUS;
+	}
+	
+	public int distanceto(Planet planet) {
+		switch(planet) {
+		case EARTH: return 0;
+		case MARS: return 7;
+		default: return 7;
+		}
+	}
+	
+	public static String mystring() {
+	      return "This is a string";
+	   }
+	
+	public double distancefrom(Planet planet) {
+		double f = 20.5;
+		Planets.Planet mars = Planet.MARS;
+	      Planets.Planet mars2 = Planet.MARS;
+	      Planets.Planet earth = Planet.EARTH;
+		switch(planet) {
+		case EARTH: return 20.5;
+		case MARS: return 14.5;
+		default: return 3.14;
+		}
+	}
+}
