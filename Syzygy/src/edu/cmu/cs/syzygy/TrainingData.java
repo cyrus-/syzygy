a2f1b9dd57a3119a558b7b0577d272f00bedd35e
Salil Joshi <salil.ssj@gmail.com>
2013-04-08 16:34:49 -0400

    really minor

--- Syzygy/src/edu/cmu/cs/syzygy/TrainingData.java
+++ Syzygy/src/edu/cmu/cs/syzygy/TrainingData.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.syzygy;
 
 public class TrainingData {
+	
 	public TrainingData() {
 		
 		
@@ -17,6 +18,21 @@ public class TrainingData {
 	}
 	
 	public int getMethodCount(SyntacticContext ctx, String type) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int getLiteralCount(SyntacticContext ctx) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getVariableCount(SyntacticContext ctx) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int getMethodCount(SyntacticContext ctx) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
