e1803a59a7c689105b4e78528c5e9a900236c1d1
Peter F Schulam <pschulam@gmail.com>
2013-04-08 17:07:07 -0400

    Add NGram class

---
+++ Syzygy/src/edu/cmu/cs/syzygy/lm/NGram.java
@@ -0,0 +1,17 @@
+package edu.cmu.cs.syzygy.lm;
+
+public class NGram {
+    
+    public NGram() {
+	
+    }
+    
+    void increment(String s) {
+	
+    }
+    
+    double prob(String s) {
+	return 0.0;
+    }
+
+}
